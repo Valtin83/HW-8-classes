b85dfd32c58e107defa8dc4922cb161a7114798b
Valentin <zhe-valentin@mail.ru>
2024-09-07 15:01:58 +0700

    HW-8 classes library card  toString,equals и hashCode

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String firstName;
     private String lastName;
@@ -7,11 +9,36 @@ public class Author {
         this.lastName = lastName;
     }
 
+    // Реализуем геттеры и сеттеры
     public String getFirstName() {
         return firstName;
     }
 
     public String getLastName() {
         return lastName;
+    }
+
+    // Реализуем метод toString
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    // Реализуем метод equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
+    }
+
+    // Реализуем метод hashCode
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
     }
 }
